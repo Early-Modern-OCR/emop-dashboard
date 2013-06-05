d786c7e20b170bdec7e06a651e8a750ae75854b3
Lou Foster <lou@performantsoftware.com>
2013-06-05 16:10:59 -0400

    wait spinner and jx visualization init stubbed in place

--- app/assets/javascripts/juxta.js
+++ app/assets/javascripts/juxta.js
@@ -1,2 +1,27 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+   var showWaitPopup = function( message ) {
+      $('#dim-overlay').show();
+      $("#wait-spinner").show();
+      $("#wait-msg").text(message);
+      $("#wait-msg").show();
+      $('#wait-popup').show();
+   };
+   
+   var hideWaitPopup = function() {
+      $('#dim-overlay').hide();
+      $("#wait-popup").hide();
+   };
+
+   var initialize = function() {  
+      hideWaitPopup();
+   };
+
+   showWaitPopup(true, "Visualizing");
+   $("body").on("sidebyside-loaded", function() {
+      initialize();
+   });
+});
+
