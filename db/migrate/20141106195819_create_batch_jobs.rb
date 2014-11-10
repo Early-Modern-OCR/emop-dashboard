240db8663ae0432bf918581021a2904147813445
Trey Dockendorf <treydock@gmail.com>
2014-11-09 21:34:21 -0600

    Set different order of columns created for batch_jobs to hopefully ease ability of importing data from legacy database

--- db/migrate/20141106195819_create_batch_jobs.rb
+++ db/migrate/20141106195819_create_batch_jobs.rb
@@ -1,12 +1,12 @@
 class CreateBatchJobs < ActiveRecord::Migration
   def change
     create_table :batch_jobs do |t|
+      t.references :job_type
+      t.references :ocr_engine
       t.string :parameters
       t.string :name
       t.string :notes
       t.references :font
-      t.references :ocr_engine
-      t.references :job_type
     end
 
     add_index :batch_jobs, :job_type_id
