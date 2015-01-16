6b56eb1e53b01e34dd100f6a516fbe4c6204be96
Trey Dockendorf <treydock@gmail.com>
2015-01-16 15:08:28 -0600

    * Fix exception notification and the way mail settings are defined

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,15 +1,20 @@
 # Initialize everything about email addresses and sending
 
+[
+  'address',
+  'port',
+  'domain',
+  'user_name',
+  'password',
+  'authentication',
+  'enable_starttls_auto',
+].each do |setting|
+  value = Rails.application.secrets.smtp_settings[setting]
+  if value.present?
+    ActionMailer::Base.smtp_settings[setting.to_sym] = value
+  end
+end
 
-ActionMailer::Base.smtp_settings = {
-	:address => Rails.application.secrets.smtp_settings['address'],
-	:port => Rails.application.secrets.smtp_settings['port'],
-	:domain => Rails.application.secrets.smtp_settings['domain'],
-	:user_name => Rails.application.secrets.smtp_settings['user_name'],
-	:password => Rails.application.secrets.smtp_settings['password'],
-	:authentication => Rails.application.secrets.smtp_settings['authentication'],
-	:enable_starttls_auto => Rails.application.secrets.smtp_settings['enable_starttls_auto']
-}
 ActionMailer::Base.default_url_options[:host] = Rails.application.secrets.smtp_settings['return_path']
 
 # This is for sendgrid: it helps with the statistics on the sendgrid site.
