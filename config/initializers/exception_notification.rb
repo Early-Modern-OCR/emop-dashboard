109e100ced6d8d27cfa0cf233e226ddb13d63ba9
Trey Dockendorf <treydock@gmail.com>
2015-02-09 18:31:12 -0600

    Add keyword 'scan' to excluded crawlers for exception notifications

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,6 @@
 if Rails.env.to_s != 'development'
   EmopDashboard::Application.config.middleware.use ExceptionNotification::Rack,
-    :ignore_crawlers => %w{Googlebot bingbot EasouSpider},
+    :ignore_crawlers => %w{Googlebot bingbot EasouSpider scan},
     :email => {
       :email_prefix => Rails.application.secrets.exception_notifier['email_prefix'],
       :sender_address => Rails.application.secrets.exception_notifier['sender_address'],
